f9abc1f1a10320cfc5948832a592f2df5ede558f
Lance Hamilton <lance.hamilton24@gmail.com>
2018-12-03 19:22:53 -0600

    console logged articles data by uid

--- src/index.js
+++ src/index.js
@@ -10,12 +10,13 @@ import './index.scss';
 import navbar from './components/Navbar/navbar';
 import loginButton from './components/Auth/auth';
 import authHelpers from './helpers/authHelpers';
+import initializeArticlesPage from './components/ArticlesPage/articlesPage';
 
 
 const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   navbar();
-  authHelpers.checkLoginStatus();
+  authHelpers.checkLoginStatus(initializeArticlesPage);
   loginButton();
 };
 
