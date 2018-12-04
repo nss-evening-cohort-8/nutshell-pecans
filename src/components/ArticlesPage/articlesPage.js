10bfc97a29c50f05c1507db1e03a974a0bfda32e
Lance Hamilton <lance.hamilton24@gmail.com>
2018-12-03 19:56:22 -0600

    printed all tasks

--- src/components/ArticlesPage/articlesPage.js
+++ src/components/ArticlesPage/articlesPage.js
@@ -1,11 +1,28 @@
+import $ from 'jquery';
 import authHelpers from '../../helpers/authHelpers';
 import articlesData from '../../helpers/data/articlesData';
+
+const articlesBuilder = (articlesArray) => {
+  articlesArray.forEach((article) => {
+    const articleString = `
+    <div class="card">
+      <div class="card-body">
+    <h2>${article.url}</h2>
+    <h2>${article.title}</h2>
+    <h2>${article.synopsis}</h2>
+    </div>
+    </div>
+  `;
+    $('#articles').append(articleString);
+  });
+};
+
 
 const articlesPage = () => {
   const uid = authHelpers.getCurrentUid();
   articlesData.getAllArticles(uid)
     .then((articlesArray) => {
-      console.log(articlesArray);
+      articlesBuilder(articlesArray);
     })
     .catch((error) => {
       console.error('error in getting articles', error);
