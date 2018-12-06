d737fbb3f6c36beded275d1e95c44f536c4ca172
Mary Rys <rys.mary@gmail.com>
2018-12-05 20:19:03 -0600

    moved zips into dropdown

--- src/components/Weather/weatherComponent.js
+++ src/components/Weather/weatherComponent.js
@@ -2,41 +2,68 @@ import $ from 'jquery';
 import authHelpers from '../../helpers/authHelpers';
 import weatherData from '../../helpers/data/weatherData';
 
-const zipStringBuilder = (weatherArray) => {
+const zipStringBuilder = (weather) => {
   let zipsBuilder = '<h3>Weather</h3>';
-  weatherArray.forEach((weather) => {
-    zipsBuilder += `<h5>${weather.zipcode}`;
+  weather.forEach((zipcodes) => {
+    zipsBuilder += `<h5>${zipcodes.zipcode}`;
   });
   return zipsBuilder;
 };
 
-const printAllZips = (weather) => {
+const printSingleZip = (weather) => {
   let cardString = `
       <div class="card">
         <div class="card-body">
+          <div class="zipContainer">${weather.zipcode}</div>
           <button class="btn btn-danger delete-weather-btn" data-weather-delete-id=${weather.id}>X</button>
           <div class="form-check form-check-inline">
           <label class="form-check-label" for="inlineCheckbox1">Current zipcode?</label>
             <input class="form-check-input checkIsCurrent" type="checkbox" id="${weather.id}">
-            <div class="zipContainer">${zipStringBuilder(weather)}</div>
           </div>
         </div>
       </div>
       `;
   $('#weather').html(cardString);
   if (weather.isCurrent) {
-    console.log('inside zip check');
     $('.checkIsCurrent').attr('checked', true);
   }
   cardString += '<button id="addZipBtn" class="btn btn-info">Add New Zipcode</button>';
   $('#weather').html(cardString);
 };
 
+const getSingleZip = (e) => {
+  const zipId = e.target.dataset.dropdownId;
+  weatherData.getSingleZip(zipId).then((singleZip) => {
+    printSingleZip(singleZip);
+  })
+    .catch((error) => {
+      console.error('error getting single zip', error);
+    });
+};
+
+const buildDropdown = (weatherArray) => {
+  let dropdown = `<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Pick a Zipcode
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">`;
+  if (weatherArray.length) {
+    weatherArray.forEach((weather) => {
+      dropdown += `<div class="dropdown-item get-single" data-dropdown-id=${weather.id}>${weather.zipcode}</div>`;
+    });
+  } else {
+    dropdown += '<div class="dropdown-item">Add a zipcode</div>';
+  }
+  dropdown += '<div></div>';
+  $('#dropdown-container').html(dropdown);
+};
+
 const weatherPage = () => {
   const uid = authHelpers.getCurrentUid();
   weatherData.getAllZips(uid)
     .then((weatherArray) => {
-      printAllZips(weatherArray);
+      buildDropdown(weatherArray);
+      zipStringBuilder(weatherArray);
     })
     .catch((error) => {
       console.error('error in getting weather', error);
@@ -69,6 +96,7 @@ const updateIsCurrent = (e) => {
 const bindEvents = () => {
   $('body').on('click', '.delete-weather-btn', deleteZips);
   $('body').on('click', '.checkIsCurrent', updateIsCurrent);
+  $('body').on('click', '.get-single', getSingleZip);
 };
 
 const initializeWeatherPage = () => {
