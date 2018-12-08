a2076d2d99b5609f620b1c2bd2cb5775b63eb419
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-12-08 12:38:31 -0600

    Is Attending Is Working

--- src/components/EventsPage/eventsPage.js
+++ src/components/EventsPage/eventsPage.js
@@ -19,56 +19,56 @@ const printAllEvents = (eventsArray) => {
          <input class="deleteButton pt-1" data-delete-id=${event.id} type="image" src="https://image.flaticon.com/icons/svg/248/248953.svg" width="30px" height="50px"></input>
             </div>`;
     $('#events').html(domString);
-    if (friend.isAvoiding) {
+    if (event.isAttending) {
       $('.is-avoiding-checkbox').attr('checked', true);
     }
   });
+};
 
-  const eventsPage = () => {
-    const uid = authHelpers.getCurrentUid();
-    eventsData.getAllEvents(uid)
-      .then((eventsArray) => {
-        printAllEvents(eventsArray);
-      }).catch((error) => {
-        console.error('error in getting tasks', error);
-      });
-  };
+const eventsPage = () => {
+  const uid = authHelpers.getCurrentUid();
+  eventsData.getAllEvents(uid)
+    .then((eventsArray) => {
+      printAllEvents(eventsArray);
+    }).catch((error) => {
+      console.error('error in getting tasks', error);
+    });
+};
 
-  const updateIsAvoiding = (e) => {
-    const eventId = e.target.id;
-    const isAvoiding = e.target.checked;
-    eventsData.updatedIsAvoiding(eventId, isAvoiding)
-      .then(() => {
+const updateIsAttending = (e) => {
+  const eventId = e.target.id;
+  const isAttending = e.target.checked;
+  eventsData.updatedIsAttending(eventId, isAttending)
+    .then(() => {
 
-      })
-      .catch((err) => {
-        console.error('error in updating flag', err);
-      });
-    console.log('you clicked checkbox');
-  };
+    })
+    .catch((err) => {
+      console.error('error in updating flag', err);
+    });
+  console.log('you clicked checkbox');
+};
 
-  const deleteEvent = (e) => {
-    // firebase id
-    const idToDelete = e.target.dataset.deleteId;
-    eventsData.deleteEvent(idToDelete)
-      .then(() => {
-        eventsPage();
-        $('#single-event-container').html('');
-      })
-      .catch((error) => {
-        console.error('error in deleting event', error);
-      });
-  };
+const deleteEvent = (e) => {
+  // firebase id
+  const idToDelete = e.target.dataset.deleteId;
+  eventsData.deleteEvent(idToDelete)
+    .then(() => {
+      eventsPage();
+      $('#single-event-container').html('');
+    })
+    .catch((error) => {
+      console.error('error in deleting event', error);
+    });
+};
 
-  const bindEvents = () => {
-    $('body').on('click', '.deleteButton', deleteEvent);
-    $('body').on('change', '.is-attending-checkbox', updateIsAvoiding);
-  };
+const bindEvents = () => {
+  $('body').on('click', '.deleteButton', deleteEvent);
+  $('body').on('change', '.is-attending-checkbox', updateIsAttending);
+};
 
-  const initializeEventsPage = () => {
-    eventsPage();
-    bindEvents();
-  };
+const initializeEventsPage = () => {
+  eventsPage();
+  bindEvents();
+};
 
-  export default initializeEventsPage;
-
+export default initializeEventsPage;
